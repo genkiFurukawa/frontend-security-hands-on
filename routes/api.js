ef1aa990abf4db19ab487e7eaabdfca884c7ef17
GENKI FURUKAWA <genki.furukawa@gmail.com>
2023-07-11 18:12:12 +0900

    [add] 4章のコードを追加

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const allowList = [
+    "http://localhost:3000",
+    "http://site.example:3000"
+];
+
+router.use((req, res, next) => {
+    // http://localhost:3000からhttp://site.example:3000/apiのリクエストを成功させつつ、その他のオリジンからのアクセスを制限したいとき
+    // => ヘッダのオリジンを確認し、オリジンが許可リストに入っているか確認する。
+    // Originヘッダが存在している、かつリクエスト許可するリスト内にOriginヘッダの情報が含まれているかチェック
+    if (req.headers.origin && allowList.includes(req.headers.origin)) {
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+    }
+    
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Headers", "X-Token");
+    }
+    next();
+});
+
 router.get("/", (req, res) => {
     res.setHeader("X-Timestamp", Date.now()); //(p.81)ヘッダをレスポンスに追加
 
